3da24a05bd1c86a2f13d773fdcd46bf6c4ab4398
Oyewale Dominion <limbotech116@gmail.com>
2026-04-19 17:43:51 +0100

    chore: add connect-react session imports

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,5 +1,9 @@
 
 import { AppConfig, UserSession } from '@stacks/connect';
+import {
+  AppConfig as ReactAppConfig,
+  UserSession as ReactUserSession,
+} from '@stacks/connect-react';
 
 export const appConfig = new AppConfig(['store_write', 'publish_data']);
 export const userSession = new UserSession({ appConfig });
