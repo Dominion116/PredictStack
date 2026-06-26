a182ad2c6abb3959c547e5809ce83536299ba05e
Dominion116 <limbotech116@gmail.com>
2026-06-26 22:25:42 +0100

    chore(api): add barrel export index for all new API clients

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,12 @@
+// Barrel export for all API clients
+export * from './client';
+export * from './feed';
+export * from './comments';
+export * from './analytics';
+export * from './categories';
+export * from './search';
+export * from './notifications';
+export * from './referrals';
+export * from './admin';
+export * from './creator';
+export * from './price-history';
