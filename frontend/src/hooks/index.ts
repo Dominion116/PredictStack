c29a5291b33082061e6859622712d4d4111d7854
Dominion116 <limbotech116@gmail.com>
2026-06-26 22:26:09 +0100

    chore(hooks): add barrel export index for all new feature hooks

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,15 @@
+// Barrel export for all new feature hooks
+export * from './use-activity-feed';
+export * from './use-comments';
+export * from './use-analytics';
+export * from './use-categories';
+export * from './use-search';
+export * from './use-notifications';
+export * from './use-referral';
+export * from './use-admin-stats';
+export * from './use-creator';
+export * from './use-price-history';
+export * from './use-theme';
+export * from './use-stx-balance';
+export * from './use-transactions';
+export * from './use-export';
